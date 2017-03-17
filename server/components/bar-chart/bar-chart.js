0d5be8b4f6e5a224b9d8dabd601cd389da11c37d
Tyler Bettilyon <teb311@gmail.com>
2017-03-17 11:00:28 -0700

    a bit more scaffolding and preperation, plus the plot actually behaves like the vertical lines I want for the test data

--- server/components/bar-chart/bar-chart.js
+++ server/components/bar-chart/bar-chart.js
@@ -29,6 +29,8 @@ class BarD3Proto extends HTMLElement {
   */
   attachedCallback() {
     this.svg = null; // We're marking intent to use this.svg
+    this.setAttribute('class', 'plotty-mc bar-chart');
+    this.innerText = " ";
 
     this.data = JSON.parse(this.getAttribute('data'));
     console.log(this.data);
@@ -53,26 +55,32 @@ class BarD3Proto extends HTMLElement {
   constructChart(barWidth) {
     // TODO: parameterize domain and range
     let heights = this.data.map(d => d.height);
-    let height = Math.max(...heights);
+    let maxHeight = Math.max(...heights);
     let width = (barWidth * this.data.length + 2 * barWidth) || 500;
+    barWidth = barWidth || width / this.data.length;
 
     // TODO: and replace below with append to svg
-    // this.svg = d3.select(this)
-    //   .append("svg")
-    //   .attr("height", height)
-    //   .attr("width", width);
+    this.svg = d3.select(this)
+      .append("svg")
+      .attr("height", maxHeight + 100) //TODO: Magic 100 padding voodoo removal
+      .attr("width", width);
 
-    d3.select(this).selectAll("div")
+
+    var bar = this.svg.selectAll("g")
         .data(this.data)
-      .enter().append("div")
-        .attr('class', "plotty-mc bar-chart")
-        // TODO: Fix so the commented out works... I want vertical bars
-        // .style("width", (barWidth || width / this.data.length) + 'px')
-        // .style("height", d => d.height + 'px')
-        .style("height", (10) + 'px')
-        .style("width", d => d.height + 'px')
-        .text(d => d.label);
+      .enter().append("g")
+        .attr("transform", (d, i) => `translate(${(barWidth + 5) * i}, 100)`); //TODO: Magic 5 padding voodoo removal
 
+    bar.append("rect")
+        .attr("y", d => maxHeight - d.height)
+        .attr("width", barWidth)
+        .attr("height", d => d.height);
+
+    bar.append("text")
+        .attr("x", (barWidth / 2) - 10) //TODO: Magic 10 padding voodoo removal
+        .attr("y", d => (maxHeight - d.height) - 10) //TODO: Magic 50 padding voodoo removal
+        // .attr("dy", ".35em")
+        .text(d => d.label);
   }
 }
 
